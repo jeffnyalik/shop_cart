5422f79ab84df7094d4d1f90beb8279e68cb473b
jeffnyalik <jeffnyak@gmail.com>
2024-03-13 23:43:40 +0300

    Finalized with paypal checkout and payments

--- src/app/components/payments/payments.component.ts
+++ src/app/components/payments/payments.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowLeftLong} from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 import { ICreateOrderRequest, IPayPalConfig, NgxPayPalModule } from 'ngx-paypal';
 import { environment } from '../../../environments/environment.development';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-payments',
@@ -18,42 +19,45 @@ import { environment } from '../../../environments/environment.development';
   templateUrl: './payments.component.html',
   styleUrl: './payments.component.css'
 })
-export class PaymentsComponent {
+export class PaymentsComponent implements OnInit, OnDestroy {
   faArrowLeftLong = faArrowLeftLong;
   isLocastorageAvailable = typeof localStorage !== 'undefined';
   getCartDetails: any = [];
-  totalNumber:number = 0;
-  showSuccess:Boolean = false;
-  showError:Boolean = false;
-  showCancel:Boolean = false;
-  details:string = '';
-  
-  public payPalConfig ? : IPayPalConfig;
+  totalNumber: number = 0;
+  showSuccess: Boolean = false;
+  showError: Boolean = false;
+  showCancel: Boolean = false;
+  details: string = '';
+  public showPaypalButtons: boolean = false;
+  public cartItem:number = 0;
+
+  public payPalConfig? : IPayPalConfig;
 
 
-  constructor(){}
+  constructor(private cartService: CartService, private router: Router) { }
   ngOnInit(): void {
-    this.checkTotal();
     this.initConfig();
+    this.checkTotal();
+    this.isLocastorageAvailable = true;
   }
 
-  cartDetails(){
-    if(this.isLocastorageAvailable){
-      if(localStorage.getItem('localCart')){
+  cartDetails() {
+    if (this.isLocastorageAvailable) {
+      if (localStorage.getItem('localCart')) {
         this.getCartDetails = JSON.parse(localStorage.getItem('localCart') || '[]');
         console.log(this.getCartDetails);
       }
     }
   }
   //check localstorage amount total
-  checkTotal(){
-    if(this.isLocastorageAvailable){
-      if(localStorage.getItem('localCart')){
+  checkTotal() {
+    if (this.isLocastorageAvailable) {
+      if (localStorage.getItem('localCart')) {
         this.getCartDetails = JSON.parse(localStorage.getItem('localCart') || '[]');
-        this.totalNumber = this.getCartDetails.reduce(function(acc:any, val:any){
+        this.totalNumber = this.getCartDetails.reduce(function (acc: any, val: any) {
           const itemAmt = parseFloat(val.price);
           const itemQnt = parseFloat(val.qnt);
-          
+
           return acc + (itemAmt * itemQnt)
         }, 0)
         console.log(this.totalNumber);
@@ -62,33 +66,34 @@ export class PaymentsComponent {
   }
   
 
-  placeOrder(){}
-  private initConfig(){
+  private initConfig(): void {
     this.payPalConfig = {
-        currency: 'EUR',
+        currency: 'USD',
         clientId: environment.clientId,
         createOrderOnClient: (data) => < ICreateOrderRequest > {
             intent: 'CAPTURE',
             purchase_units: [{
                 amount: {
-                    currency_code: 'EUR',
-                    value: '9.99',
+                    currency_code: 'USD',
+                    value: this.totalNumber.toString(),
                     breakdown: {
                         item_total: {
-                            currency_code: 'EUR',
-                            value: '9.99'
+                            currency_code: 'USD',
+                            value: this.totalNumber.toString()
                         }
                     }
                 },
-                items: [{
-                    name: 'Enterprise Subscription',
-                    quantity: '1',
+                items: this.getCartDetails.map((item:any) =>{
+                  return{
+                    name: item.name,
+                    quantity: item.qnt.toString(),
                     category: 'DIGITAL_GOODS',
                     unit_amount: {
-                        currency_code: 'EUR',
-                        value: '9.99',
+                      currency_code: 'USD',
+                      value: item.price.toString()
                     },
-                }]
+                  }
+                })
             }]
         },
         advanced: {
@@ -106,8 +111,19 @@ export class PaymentsComponent {
 
         },
         onClientAuthorization: (data) => {
-            console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+            console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', JSON.stringify(data));
             this.showSuccess = true;
+            if(this.isLocastorageAvailable){
+              if(localStorage.getItem('localCart')){
+                this.getCartDetails = [];
+                localStorage.removeItem('localCart');
+                localStorage.clear();
+                this.cartService.cartSubject.next(this.cartItem);
+                this.router.navigate(["/home"]);
+              }
+            }
+            
+
         },
         onCancel: (data, actions) => {
             console.log('OnCancel', data, actions);
@@ -124,5 +140,15 @@ export class PaymentsComponent {
         }
     };
 }
+
+  placeOrder() { 
+    this.showPaypalButtons = true;
+  }
+ 
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+  }
 
 }
